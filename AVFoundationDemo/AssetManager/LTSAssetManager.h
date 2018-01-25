23a5d0ce1bed26b7b7b720c3a019497069aeb018
LotusLee <goodboylee@126.com>
2018-01-25 14:10:39 +0800

    optimize asset manager tool

--- AVFoundationDemo/AssetManager/LTSAssetManager.h
+++ AVFoundationDemo/AssetManager/LTSAssetManager.h
@@ -10,20 +10,80 @@
 
 #define LTSAsset    [LTSAssetManager shareManager]
 
+typedef NS_ENUM(NSInteger, LTSAssetType){
+    LTSAssetTypeImage = 0,
+    LTSAssetTypeVideo
+};
 typedef void(^LTSAssetManagerBlock)(BOOL reslut);
 
+
+/**
+ 系统相册操作类
+ */
 @interface LTSAssetManager : NSObject
 
 + (instancetype)shareManager;
 
+
+/**
+ 检查用户是否授权操作相册。
+ 在操作之前建议检查，并且根据结果做相应UI逻辑处理。
+
+ @param handler 返回授权类型
+ */
 - (void)checkAuthorise:(void(^)(NSInteger authorizationStatus))handler;
 
 /**
- 添加图片到指定相册
+ 添加一张图片到指定相册
 
  @param image 图片
- @param album 相册，如果为nil则添加到系统相册
- @param block 完成回调
+ @param album 相册，nil表示添加到系统相册；不为nil，如果指定相册不存在则创建再添加.
+ @param block 结果反馈
  */
 - (void)saveImage:(UIImage *)image toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block;
+
+
+/**
+ 添加指定路径的视频到相册
+
+ @param fileURL 视频路径
+ @param album 相册  nil表示添加到系统相册；不为nil，如果指定相册不存在则创建再添加.
+ @param block 结果反馈
+ */
+- (void)saveVideoAtPath:(NSURL *)fileURL toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block;
+
+#pragma mark - 批量添加 only ios9 below
+//app支持最低ios8才可以使用以下API，建议使用
+
+
+/**
+ 根据图片路径添加单张图片到指定相册
+
+ @param fileURL 图片路径
+ @param album 相册  nil表示添加到系统相册；不为nil，如果指定相册不存在则创建再添加.
+ @param block 结果反馈
+ */
+- (void)saveImageAtPath:(NSURL *)fileURL toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block NS_AVAILABLE_IOS(8_0);
+
+/**
+ 批量添加图片到相册
+
+ @param images 元素是UIImage对象的数组
+ @param album 相册名  nil表示添加到系统相册；不为nil，如果指定相册不存在则创建再添加.
+ @param block 结果反馈
+ */
+- (void)saveImages:(NSArray <UIImage *>*)images toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block NS_AVAILABLE_IOS(8_0);
+
+
+/**
+ 批量添加图片或者视频到指定相册
+
+ @param assets 元素是NSURL对象的数组，只能是图片或者视频NSURL，两者不可共存
+ @param assetType 资源类型，图片或者视频
+ @param album 相册名  nil表示添加到系统相册；不为nil，如果指定相册不存在则创建再添加.
+ @param block 结果反馈
+ */
+- (void)saveAssets:(NSArray <NSURL *>*)assets type:(LTSAssetType)assetType toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block NS_AVAILABLE_IOS(8_0);
+
+
 @end
--- AVFoundationDemo/AssetManager/LTSAssetManager.m
+++ AVFoundationDemo/AssetManager/LTSAssetManager.m
@@ -46,8 +46,8 @@ typedef void(^SearchGroupBlock)(ALAssetsGroup *group);
 }
 
 #pragma mark - save image to album
-
-#ifdef __IPHONE_9_0
+#pragma mark - ios8 methods
+#ifdef __IPHONE_8_0
 - (void)saveImage:(UIImage *)image toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
     NSParameterAssert(image);
     self.privateBlock = block;
@@ -96,14 +96,158 @@ typedef void(^SearchGroupBlock)(ALAssetsGroup *group);
     }
 }
 
+- (void)saveImageAtPath:(NSURL *)fileURL toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
+    [self saveAssets:@[fileURL] type:LTSAssetTypeImage toAlbum:album completeHandler:block];
+}
+
+- (void)saveVideoAtPath:(NSURL *)fileURL toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
+    [self saveAssets:@[fileURL] type:LTSAssetTypeVideo toAlbum:album completeHandler:block];
+}
+
+- (void)saveImages:(NSArray <UIImage *>*)images toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
+    if (images.count == 0) {
+        return;
+    }
+
+    self.privateBlock = block;
+
+    if (album) {
+        //操作photoLibrary
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+
+            //检查指定的相册是否存在
+            PHAssetCollection *collection = [self checkAlbumWithName:album];
+
+            //创建相册变动请求
+            PHAssetCollectionChangeRequest *collectionChangeReq;
+            if (collection) {
+                collectionChangeReq = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
+            }else{
+                collectionChangeReq = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:album];
+            }
+
+            //创建相片资源
+            NSArray *placehoders = [self getAssetsWithImages:images];
+
+            //添加相片到指定相册,该api说明添加的是元素是PHAsset对象的数组，但是也可以添加元素是PHObjectPlaceholder对象的数组，文档也有演示示例
+            [collectionChangeReq addAssets:placehoders];
+
+        } completionHandler:^(BOOL success, NSError * _Nullable error) {
+            if (success && error == nil) {
+                [self callBackWithResult:YES];
+            }else{
+                [self callBackWithResult:NO];
+            }
+        }];
+    }else{
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+            for (NSInteger i = 0; i < images.count; i++) {
+                [PHAssetChangeRequest creationRequestForAssetFromImage:images[i]];
+            }
+        } completionHandler:^(BOOL success, NSError * _Nullable error) {
+            if (success && error == nil) {
+                [self callBackWithResult:YES];
+            }else{
+                [self callBackWithResult:NO];
+            }
+        }];
+    }
+}
+
+- (void)saveAssets:(NSArray <NSURL *>*)assets type:(LTSAssetType)assetType toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
+    if (assets.count == 0) {
+        return;
+    }
+    self.privateBlock = block;
+
+    if (album) {
+        //操作photoLibrary
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+
+            //检查指定的相册是否存在
+            PHAssetCollection *collection = [self checkAlbumWithName:album];
+
+            //创建相册变动请求
+            PHAssetCollectionChangeRequest *collectionChangeReq;
+            if (collection) {
+                collectionChangeReq = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
+            }else{
+                collectionChangeReq = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:album];
+            }
+
+            //创建相片资源
+            NSArray *placehoders = [self getAssetsWithURL:assets type:assetType];
+
+            //添加相片到指定相册,该api说明添加的是元素是PHAsset对象的数组，但是也可以添加元素是PHObjectPlaceholder对象的数组，文档也有演示示例
+            [collectionChangeReq addAssets:placehoders];
+
+        } completionHandler:^(BOOL success, NSError * _Nullable error) {
+            if (success && error == nil) {
+                [self callBackWithResult:YES];
+            }else{
+                [self callBackWithResult:NO];
+            }
+        }];
+    }else{
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+            if (assetType == LTSAssetTypeImage) {
+                for (NSInteger i = 0; i < assets.count; i++) {
+                    [PHAssetChangeRequest creationRequestForAssetFromImageAtFileURL:assets[i]];
+                }
+            }else{
+                //video
+                for (NSInteger i = 0; i < assets.count; i++) {
+                    [PHAssetChangeRequest creationRequestForAssetFromVideoAtFileURL:assets[i]];
+                }
+            }
+        } completionHandler:^(BOOL success, NSError * _Nullable error) {
+            if (success && error == nil) {
+                [self callBackWithResult:YES];
+            }else{
+                [self callBackWithResult:NO];
+            }
+        }];
+    }
+}
+
+- (NSArray *)getAssetsWithURL:(NSArray <NSURL *>*)urls type:(LTSAssetType)assetType{
+    NSMutableArray *tempArr = [NSMutableArray array];
+
+    if (assetType == LTSAssetTypeImage) {
+        for (NSInteger i = 0; i < urls.count; i++) {
+            PHAssetChangeRequest *assetChangeReq = [PHAssetChangeRequest creationRequestForAssetFromImageAtFileURL:urls[i]];
+            [tempArr addObject:assetChangeReq.placeholderForCreatedAsset];
+        }
+    }else{
+        //video
+        for (NSInteger i = 0; i < urls.count; i++) {
+            PHAssetChangeRequest *assetChangeReq = [PHAssetChangeRequest creationRequestForAssetFromVideoAtFileURL:urls[i]];
+            [tempArr addObject:assetChangeReq.placeholderForCreatedAsset];
+        }
+    }
+    return [tempArr copy];
+}
+
+- (NSArray *)getAssetsWithImages:(NSArray <UIImage *>*)imagas{
+    NSMutableArray *tempArr = [NSMutableArray array];
+
+    for (NSInteger i = 0; i < imagas.count; i++) {
+        PHAssetChangeRequest *assetChangeReq = [PHAssetChangeRequest creationRequestForAssetFromImage:imagas[i]];
+        [tempArr addObject:assetChangeReq.placeholderForCreatedAsset];
+    }
+
+    return [tempArr  copy];
+}
 #else
+
+#pragma mark - before ios8 methods
 - (void)saveImage:(UIImage *)image toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
     NSParameterAssert(image);
     self.privateBlock = block;
     
     if (album) {
         
-        [self checkAlbumWithName:album comletion:^(ALAssetsGroup *group) {
+        [self checkAlbumWithName:album completion:^(ALAssetsGroup *group) {
             if (group) {
                 [self saveImg:image toGroup:group];
             }else{
@@ -134,6 +278,41 @@ typedef void(^SearchGroupBlock)(ALAssetsGroup *group);
     
 }
 
+- (void)saveVideoAtPath:(NSURL *)fileURL toAlbum:(NSString *)album completeHandler:(LTSAssetManagerBlock)block{
+    NSParameterAssert(fileURL);
+    self.privateBlock = block;
+    UITableView
+    if (album) {
+        
+        [self checkAlbumWithName:album completion:^(ALAssetsGroup *group) {
+            if (group) {
+                [self saveVideo:fileURL toGroup:group];
+            }else{
+                NSLog(@"the group album isn't exist, create a new group album.");
+                __weak typeof(self) weakSelf = self;
+                [_library addAssetsGroupAlbumWithName:album resultBlock:^(ALAssetsGroup *group) {
+                    [weakSelf saveVideo:fileURL toGroup:group];
+                } failureBlock:^(NSError *error) {
+                    NSLog(@"create new group album fail.");
+                    [weakSelf callBackWithResult:NO];
+                }];
+            }
+            
+        }];
+        
+    }
+    //just write to system album "camera roll"
+    else{
+        [_library writeVideoAtPathToSavedPhotosAlbum:fileURL completionBlock:^(NSURL *assetURL, NSError *error) {
+            if (error == nil) {
+                [self callBackWithResult:YES];
+            }else{
+                [self callBackWithResult:NO];
+            }
+        }];
+    }
+}
+
 - (void)saveImg:(UIImage *)image toGroup:(ALAssetsGroup *)group{
     //保存相片到系统相册
     [_library writeImageToSavedPhotosAlbum:image.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
@@ -149,17 +328,39 @@ typedef void(^SearchGroupBlock)(ALAssetsGroup *group);
                     [self callBackWithResult:NO];
                 }
             } failureBlock:^(NSError *error) {
-                
+                NSLog(@"get the image asset url from library fail.");
+                [self callBackWithResult:NO];
             }];
         }else{
-            NSLog(@"the first step:write to system album failure.");
+            NSLog(@"the first step:save iamge to system album failure.");
+            [self callBackWithResult:NO];
+        }
+    }];
+}
+
+- (void)saveVideo:(NSURL *)videoURL toGroup:(ALAssetsGroup *)group{
+    [_library writeVideoAtPathToSavedPhotosAlbum:videoURL completionBlock:^(NSURL *assetURL, NSError *error) {
+        if (error == nil) {
+            //保存视频到指定相册
+            [_library assetForURL:assetURL resultBlock:^(ALAsset *asset) {
+                if ([group addAsset:asset]) {
+                    [self callBackWithResult:YES];
+                }else{
+                    [self callBackWithResult:NO];
+                }
+            } failureBlock:^(NSError *error) {
+                NSLog(@"get the video asset url from libray fail.");
+                [self callBackWithResult:NO];
+            }];
+        }else{
+            NSLog(@"the first step:save video to system album failure.");
             [self callBackWithResult:NO];
         }
     }];
 }
 
 //检查相册是否存在
-- (void)checkAlbumWithName:(NSString *)album comletion:(SearchGroupBlock)block{
+- (void)checkAlbumWithName:(NSString *)album completion:(SearchGroupBlock)block{
     
     __block BOOL founded = NO;
     [_library enumerateGroupsWithTypes:ALAssetsGroupAlbum | ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
